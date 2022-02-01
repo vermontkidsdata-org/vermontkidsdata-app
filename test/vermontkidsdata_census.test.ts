8688cc8c01af1da6daf8b0dbab2596538ce8fc65
Gary Bisaga <gary.bisaga@powerschool.com>
2022-02-01 07:40:22 -0500

    Initial commit

---
+++ test/vermontkidsdata_census.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as VermontkidsdataCensus from '../lib/vermontkidsdata_census-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/vermontkidsdata_census-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new VermontkidsdataCensus.VermontkidsdataCensusStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
