73a8de9434ef9711c9bfee5df6c2fbd3ff154a47
Gary Bisaga <gary.bisaga@powerschool.com>
2022-02-06 13:22:44 -0500

    initial pipeline

--- lib/vermontkidsdata_census-stack.ts
+++ lib/vermontkidsdata_census-stack.ts
@@ -1,4 +1,5 @@
 import { Stack, StackProps } from 'aws-cdk-lib';
+import { CodePipeline, CodePipelineSource, ShellStep } from 'aws-cdk-lib/pipelines';
 import { Construct } from 'constructs';
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 
@@ -6,11 +7,12 @@ export class VermontkidsdataCensusStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
-
-    // example resource
-    // const queue = new sqs.Queue(this, 'VermontkidsdataCensusQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    const pipeline = new CodePipeline(this, 'Pipeline', {
+      pipelineName: 'VKDDeployPipeline',
+      synth: new ShellStep('Synth', {
+        input: CodePipelineSource.gitHub('OWNER/REPO', 'main'),
+        commands: ['npm ci', 'npm run build', 'npx cdk synth']
+      })
+    });
   }
 }
