4aaf2315064ec33f9ba6d00cdcb09e58ee268d32
Gary Bisaga <gary.bisaga@powerschool.com>
2022-02-06 15:24:23 -0500

    change to synth env

--- lib/vermontkidsdata_census-stack.ts
+++ lib/vermontkidsdata_census-stack.ts
@@ -10,13 +10,15 @@ export class VermontkidsdataCensusStack extends Stack {
 
     const pipeline = new CodePipeline(this, 'Pipeline', {
       pipelineName: 'VKDDeployPipeline',
-      // Required to compile TypeScript with esbuild
-      dockerEnabledForSynth: true,
       synth: new ShellStep('Synth', {
         input: CodePipelineSource.connection('vermontkidsdata-org/vermontkidsdata_census', 'master', {
           connectionArn: 'arn:aws:codestar-connections:us-east-1:439348011602:connection/fd10d11a-8da2-46cf-b018-77db2b5be992'
         }),
-        commands: ['npm ci', 'npm run build', 'npx cdk synth']
+        commands: ['npm ci', 'npm run build', 'npx cdk synth'],
+        env: {
+          // Required to compile TypeScript with esbuild
+          privileged: 'true'
+        }
       })
     });
 
