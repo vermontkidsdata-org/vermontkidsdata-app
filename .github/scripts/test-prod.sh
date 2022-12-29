95a5139ef5d58fba18f28876862c60b2bafbf65b
Gary Bisaga <gbisaga@gmail.com>
2022-12-29 08:40:26 -0500

    Add prod deploy

---
+++ .github/scripts/test-prod.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+set -e
+set -x
+
+curl https://api.vtkidsdata.org/table/table/66?columns=Age,IFSP,IEP,504 >table-66.json
+
+ID=`cat table-66.json | jq -r ".id"`
+if [ "$ID" != 66 ]; then
+  exit 1
+fi
+
+TITLE=`cat table-66.json | jq -r ".metadata.config.title"`
+if [[ "$TITLE" != *"Student support services"* ]]; then
+  exit 1
+fi
+
+curl https://api.vtkidsdata.org/queries
