9eeb66df622cede0a0c13dc5639088ac219819eb
Gary Bisaga <gbisaga@gmail.com>
2023-01-02 11:59:45 -0500

    hardcode CORS

--- src/cors-config.ts
+++ src/cors-config.ts
@@ -4,21 +4,25 @@ export class CORSConfig implements Options {
   constructor(private env: NodeJS.ProcessEnv, private isPreflight?: boolean) {
   }
 
-  get origin(): string {
-    const ret = "*";
-    console.log(`CORSConfig.origin() call: returning ${ret}`);
-    return ret;
-  };
+  public origin: '*';
+  public methods: "PUT, POST, DELETE";
+  public headers: "Content-Type";
+  
+  // get origin(): string {
+  //   const ret = "*";
+  //   console.log(`CORSConfig.origin() call: returning ${ret}`);
+  //   return ret;
+  // };
 
-  get methods(): string {
-    const ret = "PUT, POST, DELETE";
-    console.log(`CORSConfig.methods() call: returning ${ret}`);
-    return ret;
-  };
+  // get methods(): string {
+  //   const ret = "PUT, POST, DELETE";
+  //   console.log(`CORSConfig.methods() call: returning ${ret}`);
+  //   return ret;
+  // };
 
-  get headers(): string {
-    const ret = "Content-Type";
-    console.log(`CORSConfig.headers() call: returning ${ret}`);
-    return ret;
-  };
+  // get headers(): string {
+  //   const ret = "Content-Type";
+  //   console.log(`CORSConfig.headers() call: returning ${ret}`);
+  //   return ret;
+  // };
 }
