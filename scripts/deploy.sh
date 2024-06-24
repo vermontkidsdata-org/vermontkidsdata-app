46926a2496ee2c8b5c0f9a3c507ad6efeb28f25e
Gary Bisaga <gary.bisaga@powerschool.com>
2024-06-24 07:53:10 -0400

    ??

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -9,7 +9,12 @@ aws --version
 npm run deploy:ci
 
 echo env=/${VKD_ENVIRONMENT}/
+echo AWS_ACCESS_KEY_ID=/${AWS_ACCESS_KEY_ID}/
+echo get secret?
 aws secretsmanager get-secret-value --secret-id "openai-config/${VKD_ENVIRONMENT}"
+
+echo set openai key?
 export OPENAI_API_KEY=$(aws secretsmanager get-secret-value --secret-id "openai-config/${VKD_ENVIRONMENT}" | jq -r '.SecretString' | jq -r ".OPENAI_API_KEY")
-echo key=$OPENAI_API_KEY
+
+echo key=/$OPENAI_API_KEY/
 npx ts-node src/sync-assistant.ts
