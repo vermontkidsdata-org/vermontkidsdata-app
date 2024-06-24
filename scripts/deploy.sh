b4d81d71da58f70cd9d0c215e0e8aa4eed429fcb
Gary Bisaga <gary.bisaga@powerschool.com>
2024-06-24 06:28:20 -0400

    debug deploy

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 set -e
+
+echo do we have jq?
+jq --version
+
 npm run deploy:ci
-export OPENAI_API_KEY=`aws secretsmanager get-secret-value --secret-id openai-config/$VKD_ENVIRONMENT | jq -r '.SecretString' | jq -r ".OPENAI_API_KEY"`
+echo env=/${VKD_ENVIRONMENT}/
+aws secretsmanager get-secret-value --secret-id "openai-config/${VKD_ENVIRONMENT}"
+export OPENAI_API_KEY=$(aws secretsmanager get-secret-value --secret-id "openai-config/${VKD_ENVIRONMENT}" | jq -r '.SecretString' | jq -r ".OPENAI_API_KEY")
 echo key=$OPENAI_API_KEY
 npx ts-node src/sync-assistant.ts
