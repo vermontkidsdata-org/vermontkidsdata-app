48fa1f9647074ba8a68db81dc9267ff7f653210d
Gary Bisaga <gbisaga@gmail.com>
2024-09-25 18:25:06 -0400

    Test timeout

--- vermontkidsdata-automation/jest.config.js
+++ vermontkidsdata-automation/jest.config.js
@@ -1,8 +1,11 @@
+const MILLISECONDS = 1000;
+
 module.exports = {
   testEnvironment: 'node',
   roots: ['<rootDir>/test'],
   testMatch: ['**/*.test.ts'],
   transform: {
     '^.+\\.tsx?$': 'ts-jest'
-  }
+  },
+  testTimeout: 60 * MILLISECONDS
 };
